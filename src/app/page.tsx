db023d62c19cfb6c3ce019279e04f88390fff827
Jerry Martinez <jaysavageg@gmail.com>
2023-07-31 10:19:15 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+
+
+      <title>Black Jack</title>
+      <link rel="stylesheet" href="css/style.css">
+      <script defer src="js/script.js"></script>
+    </div>
+  )
+}
